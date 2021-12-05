3c0ed43037ccd056c77645d11b618d75ac6b4635
Daniel <danielalmajano>
2021-12-05 23:02:12 +0100

    the basics of the objects

---
+++ src/object/objects.js
@@ -0,0 +1,15 @@
+//Forma zoomer
+var myCar1 = new Object();
+myCar1.make = 'Ford';
+myCar1.model = 'Mustang';
+myCar1.year = 1969;
+console.log(myCar1);
+
+
+let myCar = {
+    make: 'Ford',
+    model: 'Mustang',
+    year: 1969
+};
+
+console.log(myCar.make, myCar.model, myCar.year);
